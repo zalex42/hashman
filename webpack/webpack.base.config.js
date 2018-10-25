75a907458e21f9e98c611982fa72f8b1fa13b294
zalex42 <44460006+zalex42@users.noreply.github.com>
2018-10-25 15:03:59 +0700

    Add files via upload

---
+++ webpack/webpack.base.config.js
@@ -0,0 +1,34 @@
+import Config from 'webpack-config';
+import path from 'path';
+import HTMLPlugin from 'html-webpack-plugin';
+
+export default new Config().merge({
+    entry: [ 'babel-polyfill', path.resolve(__dirname, '..', 'src', 'index.js') ],
+    output: { path: path.resolve(__dirname, '..', 'dist'), publicPath: '/' },
+    resolve: {
+        extensions: ['.jsx', '.js'],
+        alias: {
+            '~': path.resolve(__dirname, '..', 'src'),
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(jpg|jpeg|svg|png|gif)/,
+                use: 'url-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new HTMLPlugin({
+            template: path.resolve(__dirname, '..', 'src', 'index.html'),
+            inject: 'body'
+        }),
+    ]
+});
