bbc36c2cc0559bc3eb7cfc003adca9901faaa3c6
zalex42 <44460006+zalex42@users.noreply.github.com>
2018-11-01 12:44:08 +0700

    Add files via upload

---
+++ src/components/Tag2/index.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+import { rem } from 'polished';
+import getColorType from '~/utils/getColorType';
+
+
+export const Group = styled.div`
+  display: flex;
+  flex-wrap: ${ props => props.inline ? 'nowrap' : 'wrap' }
+`;
+
+export default styled.span`
+  display: inline-flex;
+  margin: 2px 0;
+  padding: 0px 7px;
+
+  font-size: ${ rem('12px') };
+  font-weight: ${ props => getColorType(props.type) === 'error' ? 'bold' : 300 } !important;
+  color: ${ props => {
+    if (props.color) {
+      return props.color;
+    }
+    const type = getColorType(props.type); 
+    return type ? props.theme.notifications[type] : props.theme.colors.base.black; 
+  }};
+  
+  white-space: nowrap;
+`;
